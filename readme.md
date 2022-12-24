468b3ff845ef0118bcbacfc5fdfa0994e39bace0
abidkyo <kyotasuna95@gmail.com>
2022-12-24 01:15:35 +0100

    ENH day 9

--- readme.md
+++ readme.md
@@ -12,6 +12,7 @@
 | [06](https://adventofcode.com/2022/day/6)  | Find distinct chars           | [day06.py](src/day06.py) |
 | [07](https://adventofcode.com/2022/day/7)  | File system size              | [day07.py](src/day07.py) |
 | [08](https://adventofcode.com/2022/day/8)  | Matrix traversal              | [day08.py](src/day08.py) |
+| [09](https://adventofcode.com/2022/day/9)  | Snake                         | [day09.py](src/day09.py) |
 | [13](https://adventofcode.com/2022/day/13) | Parse different types of data | [day13.py](src/day13.py) |
 | [23](https://adventofcode.com/2022/day/23) | Moving elements in a grid     | [day23.py](src/day23.py) |
 
---
+++ src/day09.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+
+"""
+Snake.
+
+Timing:
+python3 = ~90 ms
+"""
+
+from aoc import map_list, read_input, sign
+
+# ------------------------------------------------------------------------------
+
+
+def move_snake(data: list, tail_size: int) -> set:
+    V = {"R": 1, "D": -1j, "L": -1, "U": 1j}
+
+    snake = [0] * (1 + tail_size)
+    tail_pos = set()
+
+    for direction, amount in data:
+        for _ in range(int(amount)):
+            snake[0] += V[direction]
+
+            for i, tail in enumerate(snake[1:], 1):
+                diff = snake[i - 1] - tail
+
+                if abs(diff) >= 2:
+                    tail += complex(sign(diff.real), sign(diff.imag))
+
+                snake[i] = tail
+
+            tail_pos.add(snake[-1])
+
+    return tail_pos
+
+
+def solve(day=9, test=False, testfile=""):
+    txt = read_input(day, test, testfile).splitlines()
+    data = map_list(str.split, txt)
+
+    part1 = len(move_snake(data, 1))
+    part2 = len(move_snake(data, 9))
+
+    return part1, part2
+
+
+# ------------------------------------------------------------------------------
+
+# res = solve(test=True)
+# assert res == (13, 1)
+
+res = solve()
+print(*res)
+assert res == (6406, 2643)
+
+# ------------------------------------------------------------------------------
